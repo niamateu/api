4d40ed03dd5c7ed61c3231af6ccfac6c5cbde7c3
niamateu <vfr4mju7w@gmail.com>
2024-09-04 04:34:59 +0900

    [Update] settings.py

--- config/settings.py
+++ config/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 
 DEBUG = False
 
-ALLOWED_HOSTS = ['.pythonanywhere.com', 'niamateu.pythonanywhere.com']
+ALLOWED_HOSTS = ['.pythonanywhere.com',]
 
 # Application definition
 
